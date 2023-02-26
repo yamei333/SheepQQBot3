86ab1962f7b40402ec557448df488667db8150d4
yamei <yamei.moe@gmail.com>
2023-02-26 19:23:56 +0800

    0.6.1.3 删除不使用的文件

--- SheepQQBot3.Model.bak/ClientReceiveData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SheepQQBot3.Model
-{
-    public class ClientReceiveData
-    {
-        public ClientData Data { get; set; }
-        public string RetCode { get; set; }
-        public string Status { get; set; }
-    }
-}
