993ebdabeea2387d865e64c1e78e1b62667f79a0
yamei <yamei.moe@gmail.com>
2023-02-26 19:21:07 +0800

    0.6.1.3 增加寻空的米游社API

---
+++ Xunkong.Hoyolab/HoyolabBaseWrapper.cs
@@ -0,0 +1,13 @@
+﻿namespace Xunkong.Hoyolab;
+
+internal class HoyolabBaseWrapper<T> where T : class
+{
+    [JsonPropertyName("retcode")]
+    public int ReturnCode { get; set; }
+
+    [JsonPropertyName("message")]
+    public string Message { get; set; }
+
+    [JsonPropertyName("data")]
+    public T Data { get; set; }
+}
