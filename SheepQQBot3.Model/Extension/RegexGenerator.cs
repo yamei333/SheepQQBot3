ee40ce8842a35593be61616c4aa95b012dc461b2
yamei <yamei.moe@gmail.com>
2023-05-18 16:04:25 +0800

    0.11.3.0 原神提醒增加活动和辣鸡页游提醒

--- SheepQQBot3.Model/Extension/RegexGenerator.cs
+++ SheepQQBot3.Model/Extension/RegexGenerator.cs
@@ -46,6 +46,9 @@ public static partial class RegexGenerator
     [GeneratedRegex(@"\d{4}-\d{2}-\d{2}-\d-\d (18|21):0\d:\d{2}")]
     public static partial Regex GenshinWbAlarm();
 
+    [GeneratedRegex(@"\d{4}-\d{2}-\d{2}-\d-\d (13|21):0\d:\d{2}")]
+    public static partial Regex GenshinEventAlarm();
+
     [GeneratedRegex(@"^.+?(?=\[\d{4}-\d{2}-\d{2})")]
     public static partial Regex CmdStart();
 
---
+++ SheepQQBot3.Model/GenshinHelper/GenshinEvent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace SheepQQBot3.Model;
+
+public class GenshinEventResponse
+{
+    [JsonPropertyName("data")]
+    public GenshinEventData Data { get; set; }
+}
+
+public class GenshinEventData
+{
+    [JsonPropertyName("list")]
+    public GenshinEventList[] List { get; set; }
+}
+
+public class GenshinEventList
+{
+    [JsonPropertyName("list")]
+    public GenshinEvent[] List { get; set; }
+
+    [JsonPropertyName("type_id")]
+    public int TypeId { get; set; }
+
+    [JsonPropertyName("type_label")]
+    public string TypeLabel { get; set; }
+}
+
+public class GenshinEvent
+{
+    [JsonPropertyName("ann_id")]
+    public int EventId { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("subtitle")]
+    public string SubTitle { get; set; }
+
+    [JsonPropertyName("banner")]
+    public string Banner { get; set; }
+
+    [JsonPropertyName("type_label")]
+    public string TypeLabel { get; set; }
+
+    [JsonPropertyName("tag_label")]
+    public string TagLabel { get; set; }
+
+    /// <summary>
+    /// 开始时间
+    /// </summary>
+    [JsonPropertyName("start_time")]
+    public string BeginTimeString { get; set; }
+
+    [JsonIgnore]
+    public DateTime BeginTime => Convert.ToDateTime(BeginTimeString);
+
+    public int GetDaysRemain(DateTime dateNow)
+    {
+        var totalHours = (EndTime - dateNow).TotalHours;
+        if (totalHours >= 0)
+            return (int)Math.Ceiling(totalHours / 24);
+
+        return -1;
+    }
+
+    /// <summary>
+    /// 结束时间
+    /// </summary>
+    [JsonPropertyName("end_time")]
+    public string EndTimeString { get; set; }
+
+    [JsonIgnore]
+    public DateTime EndTime => Convert.ToDateTime(EndTimeString);
+}
