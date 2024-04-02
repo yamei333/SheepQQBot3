9341a72f35e3fd17735b5cc42821011ff9a39e19
yamei <yamei.moe@gmail.com>
2024-04-02 16:30:34 +0800

    0.13.3.0 代码错误修复

---
+++ SheepQQBot3.Model/DbModel/JiebaDb/Idf.User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SheepQQBot3.DbModel.JiebaDb;
+
+public partial class Idf
+{
+    public Idf()
+    { }
+
+    public Idf(string word, decimal weight)
+    {
+        Word = word;
+        Weight = weight;
+    }
+}
